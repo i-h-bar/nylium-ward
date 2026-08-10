2f0042a992ef75eba667792ab006e14d9768b2e4
Joe Wasden <jpwaden93@gmail.com>
2026-08-10 15:56:38 +0100

    Added manifest & task helper methods

--- console.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "To exit press ctrl+P + ctrl+Q. DO NOT use ctrl+C as that will stop the server."
-docker attach minecraft-server
--- restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker stop minecraft-server
-echo "Stopped"
-
-docker start minecraft-server
-echo "Started"
